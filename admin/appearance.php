05de2f87a8f0096e0323f08c12d7780629e71ad2
FahmiYoshikage <fahmiilham029@gmail.com>
2025-11-29 23:09:44 +0700

    Fix: Replace hardcoded navbar with admin_nav.php include to show Profiles menu

--- admin/appearance.php
+++ admin/appearance.php
@@ -826,52 +826,7 @@
     </style>
 </head>
 <body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-custom">
-        <div class="container-fluid">
-            <a class="navbar-brand fw-bold" href="#">
-                <i class="bi bi-link-45deg"></i> LinkMy
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="dashboard.php">
-                            <i class="bi bi-house-fill"></i> Dashboard
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="appearance.php">
-                            <i class="bi bi-palette-fill"></i> Appearance
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="categories.php">
-                            <i class="bi bi-folder-fill"></i> Categories
-                            <span class="badge bg-success ms-1">New</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="settings.php">
-                            <i class="bi bi-gear-fill"></i> Settings
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../<?= $current_page_slug ?>" target="_blank">
-                            <i class="bi bi-eye-fill"></i> View Page
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../logout.php">
-                            <i class="bi bi-box-arrow-right"></i> Logout
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include '../partials/admin_nav.php'; ?>
     
     <!-- Loading Overlay -->
     <div class="loading-overlay" id="loadingOverlay">
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -422,51 +422,7 @@
     <!-- Drag Overlay for Mobile -->
     <div class="drag-overlay" id="dragOverlay"></div>
 
-    <nav class="navbar navbar-expand-lg navbar-custom">
-        <div class="container-fluid">
-            <a class="navbar-brand fw-bold" href="#">
-                <i class="bi bi-link-45deg"></i> LinkMy
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="dashboard.php">
-                            <i class="bi bi-house-fill"></i> Dashboard
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="appearance.php">
-                            <i class="bi bi-palette-fill"></i> Appearance
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="categories.php">
-                            <i class="bi bi-folder-fill"></i> Categories
-                            <span class="badge bg-success ms-1">New</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="settings.php">
-                            <i class="bi bi-gear-fill"></i> Settings
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../<?= $current_page_slug ?>" target="_blank">
-                            <i class="bi bi-eye-fill"></i> View Page
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../logout.php">
-                            <i class="bi bi-box-arrow-right"></i> Logout
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include '../partials/admin_nav.php'; ?>
 
     <div class="container py-4">
         <div class="row mb-4">
